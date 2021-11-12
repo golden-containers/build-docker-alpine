4dbf9ad3f485315f0c1a673bd4135ec5753a9dc1
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 13:34:01 -0800

    Clean up

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ rm -rf working
 mkdir working
 cd working
 
+GCI_URL="ghcr.io/golden-containers"
+
 # Checkout upstream
 
 git clone --depth 1 --branch v3.14 --filter=blob:none --sparse https://github.com/alpinelinux/docker-alpine.git
@@ -14,6 +16,8 @@ git sparse-checkout set x86_64
 
 # Transform
 
+GCI_REGEX_URL=$(echo ${GCI_URL} | sed 's/\//\\\//g')
+
 # This sed syntax is GNU sed specific
 # [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
 
@@ -22,11 +26,12 @@ git sparse-checkout set x86_64
 [ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
 
 BUILD_PLATFORM=" --platform linux/amd64 "
-GCI_URL="ghcr.io/golden-containers"
 BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
 
-docker build x86_64/ --tag ${GCI_URL}/alpine:latest --tag ${GCI_URL}/alpine:3.14 ${BUILD_ARGS}
-
+docker build x86_64/ ${BUILD_ARGS} \
+    --tag ${GCI_URL}/alpine:latest \
+    --tag ${GCI_URL}/alpine:3.14 
+    
 # Push
 
 docker push ${GCI_URL}/alpine -a
